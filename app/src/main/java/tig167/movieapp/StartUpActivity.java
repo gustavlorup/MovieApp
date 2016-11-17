1c407a719f9bb196e5a6e4d4b5323e8c21a86ed0
jnssonhugo <jnssonhugo@gmail.com>
2016-11-17 16:48:24 +0100

    Lagt till 3 aktiviter (login, register, start) Gjort drawable-filer till LOWERCASE
    Allting funkar atm

---
+++ app/src/main/java/tig167/movieapp/StartUpActivity.java
@@ -0,0 +1,19 @@
+package tig167.movieapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jnssonhugo on 2016-11-17.
+ */
+
+
+public class StartUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+
+    }
+}
